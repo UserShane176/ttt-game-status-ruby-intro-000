4d589f7508420bcc88bc890b2ac59dfbcf752126
Min-Hsuan Hung <poxuan33@gmail.com>
2018-08-29 06:15:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ def over?(board)
 end
 
 def winner(board)
-  WWIN_COMBINATIONS.find do |win_combo|
+  WIN_COMBINATIONS.find do |win_combo|
     if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
       return "X"
     elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
